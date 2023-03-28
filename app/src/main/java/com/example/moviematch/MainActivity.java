f8ea0539e2046ff841eca9ebad2c6194a0d31afc
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2023-03-28 17:27:24 -0300

    screenChange

--- app/src/main/java/com/example/moviematch/MainActivity.java
+++ app/src/main/java/com/example/moviematch/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.moviematch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.intro_activity);
+    }
+
+    public void loginClick(View view) {
+        Intent ScreenChange = new Intent(this, LoginActivity.class);
+        startActivity(ScreenChange);
+    }
+
+    public void registerClick(View view) {
+        Intent ScreenChange = new Intent(this, RegisterActivity.class);
+        startActivity(ScreenChange);
     }
 }
