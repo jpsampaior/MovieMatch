0d17232137369fc15a5730f675516c932fd96188
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2023-04-20 17:04:10 -0300

    updateSlide

--- app/src/main/java/com/example/moviematch/MainActivity.java
+++ app/src/main/java/com/example/moviematch/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.moviematch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,5 +33,21 @@ public class MainActivity extends AppCompatActivity {
         posterItems.add(new PosterItem(R.drawable.sample4));
 
         viewPager2.setAdapter(new MoviePosterAdapter(posterItems,viewPager2));
+        viewPager2.setClipToPadding(false);
+        viewPager2.setClipChildren(false);
+        viewPager2.setOffscreenPageLimit(3);
+        viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+
+        CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
+        compositePageTransformer.addTransformer(new MarginPageTransformer(40));
+        compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+                float r = 1 - Math.abs(position);
+                page.setScaleY(.85f + r * .15f);
+            }
+        });
+
+        viewPager2.setPageTransformer(compositePageTransformer);
     }
 }
